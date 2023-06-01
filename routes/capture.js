244af68eb58e796e51d51be6534739b041186dce
pascalecodes <pdelauna@gmail.com>
2023-06-01 01:24:35 -0700

    edit capture

--- routes/capture.js
+++ routes/capture.js
@@ -3,20 +3,7 @@ const router = express.Router();
 const upload = require("../middleware/multer");
 const captureController = require("../controllers/capture");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
-//const multer = require('multer');
-//const cloudinary = require('cloudinary').v2
-// const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
-// const storage = new CloudinaryStorage({
-//   cloudinary: require('cloudinary').v2,
-//   params: {
-//     resource_type: 'video',
-//     public_id: (req, file) => req.body.title,
-//   },
-// });
-// //const upload = multer({ dest: 'uploads/' });
-// const upload = multer({ storage: storage });
-// const fileUpload = multer()
 
 router.get("/", ensureAuth, captureController.getCapture);
 //router.get("/upload", ensureAuth, captureController.getUpload)
