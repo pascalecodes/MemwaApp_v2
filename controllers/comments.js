88aac02c1b683b36090dc56c111333cea3519ce0
pascalecodes <pdelauna@gmail.com>
2023-06-23 11:08:21 -0700

    remove comments from comments js

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,15 +1,12 @@
 const Comment = require("../models/Comment");
-//const User = require("../models/User");
 
 module.exports = {
   createComment: async (req, res) => {
     try {
-      // const commentUser = await User.findById(req.user.id)
       await Comment.create({
         comment: req.body.comment,
         likes: 0,
         post: req.params.id,
-        // madeBy: commentUser.userName,
         madeBy: req.user.userName,
         madeByID: req.user.id
       });
@@ -23,9 +20,6 @@ module.exports = {
   deleteComment: async (req, res) => {
     try {
       // Find comment by id
-      //let comment = await Comment.findById({ _id: req.params.id });
-      // Delete comment from db
-      //await Comment.remove({ _id: req.params.id });
       await Comment.deleteOne({_id: req.params.commentid})
       console.log("Deleted Comment");
       res.redirect("/post/"+req.params.postid)
