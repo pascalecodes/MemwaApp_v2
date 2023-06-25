c6098cce8179dc67a91a888909e804db2579d7d9
pascalecodes <pdelauna@gmail.com>
2023-06-25 14:49:29 -0700

    remove comment home js

--- controllers/home.js
+++ controllers/home.js
@@ -5,9 +5,6 @@ const path = require('path')
 module.exports = {
   getIndex: async (req, res) => {
     try {
-      // if(ensureAuth){
-      //   res.render("feed.ejs");
-      // }
       res.render("index");
     } catch(err) {
       console.log(err)
@@ -16,13 +13,8 @@ module.exports = {
   },
   getHome: async (req, res) => {
     try {
-      // if(ensureAuth){
-      //   res.render("feed.ejs");
-      // }
       const posts = await Post.find().sort({ createdAt: "desc" }).populate('user').lean();
       const ext= posts.map(post=> path.extname(post.media))
-      // console.log(ext)
-      //res.render("feed.ejs", { posts: posts });
       res.render("home.ejs", {posts: posts, user: req.user, ext: ext});
     } catch(err) {
       console.log(err)
@@ -31,36 +23,9 @@ module.exports = {
   },
   getWatch: async (req, res) => {
     try {
-      // if(ensureAuth){
-      //   res.render("feed.ejs");
-      // }
       const posts = await Post.find().sort({ createdAt: "desc" }).populate('user').lean();
       const ext= posts.map(post=> path.extname(post.media))
-      //const playlist = posts.map(post => `${post.title} by ${post.user.userName}`)
       const playlist = posts.map(post => [post._id, post.title, post.user.userName, post.media])
-      //const playlist = posts.forEach(post => {title: post.title, media: post.media, user: post.userName})
-
-      // const video_list = function(req, res) {
-      //   Post.find()
-      //     .exec(function(err, playlist) {
-      //       if (err) { return next(err); }
-      //       res.render('watch.ejs', { title: 'Video Playlist', playlist: playlist });
-      //     });
-      // };
-      
-      // const video_detail = function(req, res) {
-      //   Post.findById(req.params.id)
-      //     .exec(function(err, video) {
-      //       if (err) { return next(err); }
-      //       res.render('post.ejs', { posts: posts, user: req.user});
-      //     });
-      // };
-
-      //res.render("feed.ejs", { posts: posts });
-      // console.log(posts)
-      // console.log(ext)
-      // console.log(playlist)
-      // console.log(video_list)
       res.render("watch.ejs", {posts: posts, user: req.user, ext: req.ext, playlist: playlist});
     } catch(err) {
       console.log(err)
@@ -74,8 +39,6 @@ module.exports = {
       
       const video_list = await 
         Post.find().populate('user')
-        //const ext= video_list.map(video=> path.extname(video.media))
-  
         const video_detail = []; // initialize empty playlist array
 
         video_list.forEach(video => {
@@ -88,7 +51,6 @@ module.exports = {
             ext: path.extname(video.media)
           }); // add video objects to playlist array
         });
-      // console.log( video_detail)
       res.render('watch.ejs', {posts: posts, videos: video_list, detail: video_detail, user: req.user})
   
     } catch(err) {
