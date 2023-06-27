547d5b20c55417af8260b9c35c74f7efaea80120
pascalecodes <pdelauna@gmail.com>
2023-06-26 22:08:37 -0700

    remove comment from post js controller

--- controllers/posts.js
+++ controllers/posts.js
@@ -17,70 +17,32 @@ module.exports = {
     }
   },
   uploadAvatar: async (req, res) => {
-    //const {id} = req.params.id;
     let user = req.user
     let id = req.params.id;
     const {userName, email, firstName, lastName, avatar} = req.body;
     try {
       const result = await cloudinary.uploader.upload(req.file.path, {resource_type: "auto"})
-      //console.log(result)
-      //let avatar = result.secure_url
-      //console.log(avatar)
-      //const user = await User.findOne({ _id: id})
       user.avatar = result.secure_url
       user.save()
-      //user = await User.findByIdAndUpdate(id, { avatar}, { new: true })
-      //user.avatar = result.secure_url;
-      //await user.save();
-
-      // try {
-      //     const user = req.user
-      //     const result = await cloudinary.uploader.upload(req.file.path, {resource_type: "auto"})
-      //     console.log(result)
-    
-      //     user.avatar = result.public_id
-      //     //user.save()
-      //     console.log(user.avatar)
       res.redirect("/profile");
     } catch (err) {
       console.log(err);
       res.render('error/500')
     }
-    // await User.save({
-    //   avatar: result.secure_url,
-    // });
-    // console.log("Post has been added!");
-
-    // try {
-    //   const user = req.user
-    //   const result = await cloudinary.uploader.upload(req.file.path, {resource_type: "auto"})
-    //   console.log(result)
-
-    //   user.avatar = result.public_id
-    //   //user.save()
-    //   console.log(user.avatar)
-    //  res.render('editProfile.ejs', {user: user})
-    // } catch(err){
-    //   console.log(err)
-    //   res.render('error/500')
-    // }
-    
-
   },
   updateProfile: async (req,res) => {
     try {
       console.log(req.params.id)
       let user= req.user
       let id = user._id
-      // console.log(id)
-        let {
+        
+      let {
           firstName,
           lastName,
           email,
           bio,
           //avatar,
         } = req.body
-        //console.log('current info:', req.body)
       
         let updateUser = {
           firstName:  firstName= (req.body.firstName? req.body.firstName: req.user.firstName),
@@ -89,7 +51,6 @@ module.exports = {
           bio:  bio= (req.body.bio? req.body.bio: req.user.bio),
           //avatar: avatar,
         }
-        //console.log('fields in form:', updateUser)
         user = await User.findByIdAndUpdate(id, { firstName, lastName, email, bio }, { new: true });
         console.log("Profile has been edited!");
         res.redirect('/profile')
@@ -101,8 +62,6 @@ module.exports = {
   editProfile: async (req,res) => {
     try {
       const user = req.user;
-      //console.log(user)
-      //console.log(user._id)
       res.render('editProfile.ejs', {
         user,
       });
@@ -114,17 +73,14 @@ module.exports = {
   },
   getFeed: async (req, res) => {
     try {
-      //const posts = await Post.find().sort({ createdAt: "desc" }).populate('user').lean();
       const posts = await Post.find().sort({ likes: "desc" }).populate('user');
       const ext= posts.map(post=> path.extname(post.media))
-      //console.log(posts.user.userName)
     
       //group posts by username but not using it to render right now
       const groupPosts = posts.reduce((acc, post) => {
         if (!acc[post.user.userName]) {
           acc[post.user.userName] = []
         }
-        //acc[post.user.userName].push(post);
         acc[post.user.userName].push({
           ...post,
           ext: path.extname(post.media)
@@ -132,9 +88,7 @@ module.exports = {
         return acc;
       }, {});
 
-      //console.log(groupPosts);
-      console.log(posts)
-      //res.render("feed.ejs", { posts: posts,  user: req.user, ext: ext}); 
+      //console.log(posts)
       res.render("feed.ejs", { posts: posts,  user: req.user, ext: ext, groupPosts: groupPosts});
     } catch (err) {
       console.log(err);
@@ -144,10 +98,6 @@ module.exports = {
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id).populate('user');
-      // const comments = await Comment.find(post+req.params.id).sort({ createdAt: "desc" }).lean();
-      //const users = await User.findById(req.params._id)
-    //   const userName = await User.find({_id: req.params.userName})
-    //console.log(post)
       const ext= path.extname(post.media)
     
       const comments = await Comment.find({post: req.params.id}).sort({ createdAt: "desc" }).lean();
@@ -157,21 +107,6 @@ module.exports = {
       res.render('error/500')
     }
   },
-  // findPost: async (req, res) => {
-  //   try {
-  //     // const {postName} =req.query
-  //     // const posts = await Post.find({title: postName})
-  //     const postID = req.query
-  //     console.log(postID)
-  //     const posts = await Post.findById(postID)
-  //     // const ext= posts.map(post=> path.extname(post.media))
-      
-  //     res.render("find.ejs", postID);
-  //   } catch (err) {
-  //     console.log(err);
-  //     res.render('error/500')
-  //   }
-  // },
   getAdd: async (req, res) => {
     try {
       res.render("stories/add.ejs")
@@ -182,10 +117,6 @@ module.exports = {
   },
   createPost: async (req, res) => {
     try {
-      //Upload image to cloudinary
-      // const result = await cloudinary.uploader.upload(req.file.path, {
-      //   resource_type: "auto", folder: "memwa",
-      // });
       const result = await cloudinary.uploader.upload(req.file.path, {resource_type: "auto"});
       
       await Post.create({
